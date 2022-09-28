6370a2decc75dbaaa6361eeec05718454808953d
sourbiskit <ruanvictor.qa@gmail.com>
2022-09-28 12:06:19 -0300

    Criação da aplicação e base dos modelos

---
+++ take5/survey/models.py
@@ -0,0 +1,18 @@
+from django.conf import settings
+from django.db import models
+
+
+class Survey(models.Model):
+    pass
+
+
+class SurveyQuestion(models.Model):
+    pass
+
+
+class SurveyQuestionAlternative(models.Model):
+    pass
+
+
+class SurveyUserQuestion(models.Model):
+    pass
