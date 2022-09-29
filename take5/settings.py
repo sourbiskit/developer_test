83962ba6cec21b9fa776b6bd7da05e2d5be12946
sourbiskit <ruanvictor.qa@gmail.com>
2022-09-28 21:29:17 -0300

    Migração atualizada dos modelos

--- take5/settings.py
+++ take5/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'survey.apps.SurveyConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
